35947dddc76f915bed13735c3a469a53aa2b24fc
amir <amirtaki623@gmail.com>
2025-07-22 23:52:37 +0330

    querySelect at project

---
+++ src/main.js
@@ -0,0 +1,8 @@
+const formOpenBtn = document.querySelector('#form-open')
+const home = document.querySelector('.home')
+const formContainer = document.querySelector('.form_container')
+const formCloseBtn = document.querySelector('.form_close')
+const signupBtn = document.querySelector('#signup')
+const loginupBtn = document.querySelector('#loginup')
+const  pwShowHide = document.querySelectorAll('.pw_hide')
+
