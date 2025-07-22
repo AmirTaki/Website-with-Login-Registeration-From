be3163b47688a0434825f7ee826987b72bf2090f
amir <amirtaki623@gmail.com>
2025-07-23 00:20:50 +0330

    complete project

--- src/main.js
+++ src/main.js
@@ -27,5 +27,13 @@ loginupBtn.addEventListener('click', (e)=>{
 pwShowHide.forEach((icon)=>{
     icon.addEventListener("click", (e) => {
         let getPwInput =  icon.parentElement.querySelector("input")
+        if(getPwInput.type === "password"){
+            getPwInput.type = "text"
+            icon.classList.replace("uil-eye-slash", "uil-eye")
+        }
+        else {
+            getPwInput.type = "password"
+            icon.classList.replace("uil-eye", "uil-eye-slash")
+        }
     })
 })
